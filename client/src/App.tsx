94da21291498bf7d6c4fc3ff25dc8cc77ca874aa
matterai <matterai.net@gmail.com>
2022-07-08 23:48:31 +0300

    Reinstalled client with react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
+function App() {
   return (
     <div>
-      <p>Hello there!</p>
+      <p>Hello world!</p>
     </div>
   );
 }
